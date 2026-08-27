41745cbf63dc649922505a2dd2f0c43537470626
Sudarshan Patil <mr.sudarsh@gmail.com>
2026-08-27 12:28:30 +0530

    Day 2

---
+++ Day 2/TriangleProgram.java
@@ -0,0 +1,16 @@
+class TriangleProgramm
+{
+	public static void main(String args[]){
+		var num = 5;
+		for(int i=1; i<=num; i++)
+		{
+			var str = "";
+			for(int j=1; j<=i; j++)
+			{
+				str += ""; 
+			}
+			System.out.println(str);
+			str = "";
+		}
+	}
+}
---
+++ Day 2/Welcome.java
@@ -0,0 +1,16 @@
+class Greeting 
+{
+	public static void main(String args[]) 
+	{
+		System.out.println("Hello, Welcome Everyone!!!");
+		System.out.println("Hey, This is producing semicolon error");
+	}
+}
+
+public class Session 
+{
+	public static void main(String args[]) 
+	{
+		System.out.println("This is the session class");
+	}
+}
