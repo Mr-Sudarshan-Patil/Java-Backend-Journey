7339d1cce812a93b76248c52034cefad5f2b0b90
Sudarshan Patil <mr.sudarsh@gmail.com>
2026-08-28 20:27:37 +0530

    Day 3: Second Commit

---
+++ Day 3/HospitalDetails.java
@@ -0,0 +1,22 @@
+public class HospitalDetails
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hospital Name: LIFE CARE HOSPITAL");
+		System.out.println("Hospital City: Bangalore");
+		System.out.println("Hospital Contact: 9453647643");
+		System.out.println("Ambulance No: 112");
+	}
+}
+
+class Patient1Detail
+{
+	public static void main(String args[])
+	{
+		System.out.println("===============Details About Patient=========");
+		System.out.println("Doctor Handling This patient: Dr. Kamran");
+		System.out.println("Patient Name: Aditya Kumar");
+		System.out.println("Patient in ICU");
+		System.out.print("Patient Dead after doing operation by Dr. Kamran Ahmad");
+	}
+}
---
+++ Day 3/MobileInfo.java
@@ -0,0 +1,19 @@
+public class MobileInfo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Brand: Realme");
+		System.out.println("Model: X7 Max");
+		System.out.println("Price: 32000");
+		System.out.println("Color: Black");
+		System.out.println("Storage: 128GB");
+	}
+}
+
+
+class IphoneMobileDetails 
+{
+	public static void main(String args[]){
+		System.out.println("Iphone nahi hai mere pass, to me bata nahi sakta uska details");
+	}
+}
