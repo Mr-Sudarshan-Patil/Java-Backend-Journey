97b2d5e9368bb3adc7a3968b4054dea5ede95526
Sudarshan Patil <mr.sudarsh@gmail.com>
2026-09-02 11:56:55 +0530

    Date: 02-09-2026

---
+++ Day 1/HelloWorld.java
@@ -0,0 +1,5 @@
+3public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Kodewala...");
+	}
+}
---
+++ Extra Practice Question/LoopClass.java
@@ -0,0 +1,10 @@
+public class LoopClass
+{
+	public static void main(String args[])
+	{
+		for(byte i = 1; 1>=0; i++) 
+		{
+			System.out.println(i);
+		}
+	}
+}
