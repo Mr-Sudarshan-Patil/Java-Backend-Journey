e09017d40525092329b5dc47f9a2aff9d7ede9b3
Sudarshan Patil <mr.sudarsh@gmail.com>
2026-09-03 15:09:28 +0530

    Date: 03-09-2026

---
+++ Extra Practice Question/ArithmaticOperation.java
@@ -0,0 +1,32 @@
+public class ArithmaticOperation
+{
+	public static void main(String args[]){
+		int a = 5;
+		int b = 6;
+		
+		System.out.println("==============Addition===============");
+		
+		int sum = a+b;
+		
+		System.out.println("Addition of A+B is :" + sum);
+		
+		System.out.println("=============Substraction================");
+		
+		int sub = a-b;
+		
+		System.out.println("Substraction A-B is :" + sub);
+		
+		System.out.println("==============Multiplication===============");
+		
+		int mul = a*b;
+		
+		System.out.println("Multiplication A*B is: " + mul);
+		
+		System.out.println("==========Division==============");
+		
+		int div = a / b;
+		
+		System.out.println("Division A / B is: " + div);
+		
+	}
+}
--- Extra Practice Question/SumConcatination.java
+++ Extra Practice Question/SumConcatination.java
@@ -7,15 +7,22 @@ public class SumConcatination
 		int sum = a + b;
 		System.out.println("Sum of " + a + " and " + b + " is " + sum); // with the help of + operater we are doing the concatinations in string and integer
 	}
+	
+	
+	
+					/*
+
+					Rules of concatination:
+
+					integer + integer = integer  e.g  2 + 3 = 5
+					integer + string = string	e.g   5 + "Hello" = "5Hello"
+					string + integer = string   e.g   "Hello" + 10 = "Hello10"
+					string + string = string   e.g    "Hello" + "World" = "HelloWorld"
+
+					*/
+					
+					
+
+					
+					
 }
-
-/*
-
-Rules of concatination:
-
-integer + integer = integer  e.g  2 + 3 = 5
-integer + string = string	e.g   5 + "Hello" = "5Hello"
-string + integer = string   e.g   "Hello" + 10 = "Hello10"
-string + string = string   e.g    "Hello" + "World" = "HelloWorld"
-
-*/
