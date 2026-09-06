727cd728944d0116f468f64aae9cbd0ac7a1fab4
Sudarshan Patil <mr.sudarsh@gmail.com>
2026-09-06 23:12:22 +0530

    commit

---
+++ Extra Practice Question/Inputs/LargestNumber.java
@@ -0,0 +1,23 @@
+//Quetion take a 3 integers from the input and find the largest number in 3 numbers
+import java.util.Scanner;
+
+public class LargestNumber
+{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
+		
+		if(num1 > num2 && num1 > num3) {
+			System.out.println("The largest number is num1 : " + num1);
+		}
+		else if(num2 > num1 && num2 > num3){
+			System.out.println("The largest number is num2: " + num2);
+		}
+		else{
+			System.out.println("The largest number is num3: " + num3);
+		}
+	}
+}
